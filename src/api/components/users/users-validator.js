046ccf84f4b169d167e51bf400e85eec85356f1f
unknown <della.535230190@stu.untar.ac.id>
2024-04-11 22:35:39 +0700

    535230190_DellaStngrm_ExerciseApiProject

---
+++ src/api/components/users/users-validator.js
@@ -0,0 +1,32 @@
+const joi = require('joi');
+
+module.exports = {
+  createUser: {
+    body: {
+      name: joi.string().min(1).max(100).required().label('Name'),
+      email: joi.string().email().required().label('Email'),
+      password: joi.string().min(6).max(32).required().label('Password'),
+      password_confirm: joi
+        .string()
+        .min(6)
+        .max(32)
+        .required()
+        .label('Password'),
+    },
+  },
+
+  updateUser: {
+    body: {
+      name: joi.string().min(1).max(100).required().label('Name'),
+      email: joi.string().email().required().label('Email'),
+    },
+  },
+
+  changePassword: {
+    body: {
+      old_password: joi.string().min(6).max(32).required(),
+      new_password: joi.string().min(6).max(32).required(), // contoh: password harus memiliki panjang minimal 8 karakter dan maksimal 30 karakter
+      confirm_new_password: joi.string().min(6).max(32).required(),
+    },
+  },
+};
